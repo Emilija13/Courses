54f28941ebba27aca65197775a6fbc0ee51c0169
Emilija13 <emilijalakinska13@gmail.com>
2025-06-17 08:28:59 +0200

    login an logout endpoint

---
+++ courses-app.server/app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+use App\Http\Requests\LoginRequest;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function login(LoginRequest $request)
+    {
+        $data = $request->validated();
+
+        if(!Auth::attempt($data)){
+            return response([
+                'message' => 'email or password are wrong'
+            ]);
+        }
+        $user = Auth::user();
+
+        $token = $user->createToken('main')->plainTextToken;
+
+        return response([
+            'user'  => $user,
+            'token' => $token,
+        ], 201);
+    }
+
+    public function logout(Request $request)
+    {
+        $user = $request->user();
+        if ($user) {
+            $user->currentAccessToken()->delete();
+            return response('', 204);
+        }
+
+    return response(['error' => 'Not authenticated'], 401);
+    }
+}
--- courses-app.server/routes/api.php
+++ courses-app.server/routes/api.php
@@ -4,6 +4,11 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\StudentController;
 use App\Http\Controllers\Api\ProfessorController;
 use App\Http\Controllers\Api\CourseController;
+use App\Http\Controllers\Api\AuthController;
+
+
+Route::post('login', [AuthController::class, 'login']);
+Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
 
 Route::apiResource('students', StudentController::class);
 Route::get('students/{student}/courses', [StudentController::class, 'courses']);
